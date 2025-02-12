d35f7def2420d6d9ba5a5ecda0c39feec8a27925
Siarhei <t375336306588@gmail.com>
2025-02-12 19:31:45 +0300

    first commit

---
+++ Counter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Counter
+//
+//  Created by Сергей Абушкевич on 12.02.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
