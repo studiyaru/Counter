f178fd29b4e7f38a0f1d7e679e28a17f2eceb3ff
studiyaru <sergey.a@studiya.ru>
2025-02-15 14:56:43 +0300

    first commit

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var btn: UIButton!
+    @IBOutlet weak var label: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    @IBAction func increase(_ sender: Any) {
+        var count = Int(label.text ?? "0")
+        
+        label.text = String(count! + 1)
+        
+    }
+    
 
 }
 
